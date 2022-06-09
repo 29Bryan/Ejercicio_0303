5086d1ce0d29db810ef815d45a26dd346fefe96b
PCG <PCG@DESKTOP-CQA7U9O>
2022-06-08 19:55:44 -0500

    Realizacion Ejercicio 0303

--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -9,5 +9,64 @@ package modelo;
  * @author PCG
  */
 public class Estudiante {
+    private String nombre; 
+    private int edad; 
+    private int numeroCedula; 
+    private String lugarNacimiento; 
+    private String grado; 
+
+    public Estudiante(String nombre, int edad, int numeroCedula, String lugarNacimiento, String grado) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.numeroCedula = numeroCedula;
+        this.lugarNacimiento = lugarNacimiento;
+        this.grado = grado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getNumeroCedula() {
+        return numeroCedula;
+    }
+
+    public void setNumeroCedula(int numeroCedula) {
+        this.numeroCedula = numeroCedula;
+    }
+
+    public String getLugarNacimiento() {
+        return lugarNacimiento;
+    }
+
+    public void setLugarNacimiento(String lugarNacimiento) {
+        this.lugarNacimiento = lugarNacimiento;
+    }
+
+    public String getGrado() {
+        return grado;
+    }
+
+    public void setGrado(String grado) {
+        this.grado = grado;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "nombre=" + nombre + ", edad=" + edad + ", numeroCedula=" + numeroCedula + ", lugarNacimiento=" + lugarNacimiento + ", grado=" + grado + '}';
+    }
+    
     
 }
